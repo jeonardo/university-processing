4cde59ee67166c400008e501afa66f39fe279f6f
jeonardo <87972563+jeonardo@users.noreply.github.com>
2024-05-01 15:59:26 +0300

    Code cleanup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Grduate processing API
+
+## Info:
+
+The application helps to automize some part of an university's internal processes
+according to a graduating process. It consists of several parts:
+
+* deals between students and teachers
+* accepting graduating works title
+* planning of an exams
---
+++ src/UniversityProcessing.DomainServices/DomainServicesRegistrar.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UniversityProcessing.DomainServices.Core;
+
+namespace UniversityProcessing.DomainServices;
+
+public static class DomainServicesRegistrar
+{
+    public static void Configure(IConfiguration configuration, IServiceCollection services)
+    {
+        services.RegisterRequestHandlers();
+
+        services.AddSingleton<ITokenService, TokenService>();
+    }
+
+    private static IServiceCollection RegisterRequestHandlers(this IServiceCollection services)
+    {
+        return services.AddMediatR(
+            x =>
+                x.RegisterServicesFromAssembly(typeof(DomainServicesRegistrar).Assembly));
+    }
+}
